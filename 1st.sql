6d92f92ee8fe7d94932a3a068d0687e8d8b5a5a5
Tolulope Abolarin <Tolulopeabolarin85@yahoo.com>
2022-02-18 12:29:15 +0100

    Added a new query

---
+++ 1st.sql
@@ -0,0 +1,26 @@
+select [Name] ='Tolulope', 26 as [Age], 'Oke Afa'  [Address]
+;
+
+select [Sales Channel] from [Tolu].[dbo].[SALES_DATA]
+
+
+select [Sales Channel]
+from [Tolu].[dbo].[SALES_DATA]
+where [Distributor ID] = 23265 [Distributor ID] = 23378 [Distributor ID] = 23288 [Distributor ID] = 23347
+;
+
+
+
+
+
+
+
+
+
+
+
+23265
+23378
+23288
+23347
+
